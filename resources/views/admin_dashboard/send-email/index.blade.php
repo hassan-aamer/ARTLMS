53a287d53ac980bb801810492a63ec4e5f5d62de
hassan-aamer <hassanaamer048@gmail.com>
2024-05-21 04:45:39 +0300

    "Refactored HTML structure and formatting in admin_dashboard/send-email/index.blade.php"

--- resources/views/admin_dashboard/send-email/index.blade.php
+++ resources/views/admin_dashboard/send-email/index.blade.php
@@ -9,8 +9,10 @@
             <div class="card">
                 <div class="card-body">
                     <div class="d-flex align-items-center">
-                        <h5 class="mb-0"> <i class="bi bi-grid-fill"></i> رسالة من <small
-                                class="text-success">({{ $contactMessage->name }})</small> </h5>
+                        <h5 class="mb-0"> <i class="bi bi-grid-fill"></i> رسالة من
+                            <smallclass="text-success">({{ $contactMessage->name }}) أختار من القائمه من الذى يمكنك ارسال
+                            الرسالة الية</smallclass=>
+                        </h5>
                     </div>
                     <div class="row g-3 mt-4">
                         <div class="col-12">
@@ -46,93 +48,92 @@
     </div>
 
     {{-- -------------------------------------- المحاضرون و المعلمون ------------------------------- --}}
-<form action="{{ route('sendEmail.store') }}" method="POST">
-    @csrf
-    <input type="hidden" name="contactMessage" value="{{ $contactMessage->id }}">
-    <div class="card">
-        <div class="card-body">
-            <div class="d-flex align-items-center">
-                <h5 class="mb-0"> <i class="bi bi-grid-fill"></i>  المحاضرون و المعلمون </h5>
-            </div>
-            <div class="table-responsive mt-4">
-                <table class="table align-middle">
-                    <thead class="table-secondary">
-                    <tr>
-                        <th>#</th>
-                        <th>الاسم</th>
-                        <th>البريد الإلكتروني</th>
-                        <th>التحكم</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        @forelse($teacher as $con)
-                        <tr>
-                            <td>{{$con->id}}</td>
-                            <td>{{$con->name}}</td>
-                            <td>{{$con->email}}</td>
-                            <td>
-                                <input type="checkbox" name="teachers[]" value="{{ $con->id }}">
-                            </td>
-                        </tr>
-                        @empty
+    <form action="{{ route('sendEmail.store') }}" method="POST">
+        @csrf
+        <input type="hidden" name="contactMessage" value="{{ $contactMessage->id }}">
+        <div class="card">
+            <div class="card-body">
+                <div class="d-flex align-items-center">
+                    <h5 class="mb-0"> <i class="bi bi-grid-fill"></i> المحاضرون و المعلمون </h5>
+                </div>
+                <div class="table-responsive mt-4">
+                    <table class="table align-middle">
+                        <thead class="table-secondary">
                             <tr>
-                                <td colspan="6" class="text-center">
-                                    <p> لا يوجد بيانات </p>
-                                </td>
+                                <th>#</th>
+                                <th>الاسم</th>
+                                <th>البريد الإلكتروني</th>
+                                <th>التحكم</th>
                             </tr>
-                        @endforelse
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @forelse($teacher as $con)
+                                <tr>
+                                    <td>{{ $con->id }}</td>
+                                    <td>{{ $con->name }}</td>
+                                    <td>{{ $con->email }}</td>
+                                    <td>
+                                        <input type="checkbox" name="teachers[]" value="{{ $con->id }}">
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="6" class="text-center">
+                                        <p> لا يوجد بيانات </p>
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-    {{-- ------------------------------------المتعلمين--------------------------------- --}}
+        {{-- ------------------------------------المتعلمين--------------------------------- --}}
 
-    <div class="card">
-        <div class="card-body">
-            <div class="d-flex align-items-center">
-                <h5 class="mb-0"> <i class="bi bi-grid-fill"></i>  المتعلمين </h5>
-            </div>
-            <div class="table-responsive mt-4">
-                <table class="table align-middle">
-                    <thead class="table-secondary">
-                    <tr>
-                        <th>#</th>
-                        <th>الاسم</th>
-                        <th>البريد الإلكتروني</th>
-                        <th>التحكم</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        @forelse($student as $con)
-                        <tr>
-                            <td>{{$con->id}}</td>
-                            <td>{{$con->name}}</td>
-                            <td>{{$con->email}}</td>
-                            <td>
-                                <input type="checkbox" name="students[]" value="{{ $con->id }}">
-                            </td>
-                        </tr>
-                        @empty
+        <div class="card">
+            <div class="card-body">
+                <div class="d-flex align-items-center">
+                    <h5 class="mb-0"> <i class="bi bi-grid-fill"></i> المتعلمين </h5>
+                </div>
+                <div class="table-responsive mt-4">
+                    <table class="table align-middle">
+                        <thead class="table-secondary">
                             <tr>
-                                <td colspan="6" class="text-center">
-                                    <p> لا يوجد بيانات </p>
-                                </td>
+                                <th>#</th>
+                                <th>الاسم</th>
+                                <th>البريد الإلكتروني</th>
+                                <th>التحكم</th>
                             </tr>
-                        @endforelse
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @forelse($student as $con)
+                                <tr>
+                                    <td>{{ $con->id }}</td>
+                                    <td>{{ $con->name }}</td>
+                                    <td>{{ $con->email }}</td>
+                                    <td>
+                                        <input type="checkbox" name="students[]" value="{{ $con->id }}">
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="6" class="text-center">
+                                        <p> لا يوجد بيانات </p>
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-    {{-- -------------------------------------------------------------------- --}}
-    <div class="row">
-        <div class="col-lg-12 mx-auto">
-            <div class="card">
+        {{-- -------------------------------------------------------------------- --}}
+        <div class="row">
+            <div class="col-lg-12 mx-auto">
+                <div class="card">
                     <button class="btn btn-primary" type="submit"> ارسال</button>
+                </div>
             </div>
         </div>
-    </div>
-</form>
+    </form>
 @endsection
-
