1e14b7a725f6bc57326b756f08aba7824a236a7a
hassan-aamer <hassanaamer048@gmail.com>
2024-05-21 15:32:04 +0300

    Added nullable 'file' column to 'contacts' table.

--- database/migrations/2023_08_06_131258_create_contacts_table.php
+++ database/migrations/2023_08_06_131258_create_contacts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('phone');
             $table->text('subject');
             $table->text('message');
+            $table->string('file')->nullable();
             $table->timestamps();
         });
     }
