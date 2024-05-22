62356f5a52d958afe5c21a15c2135d988e360303
hassan-aamer <hassanaamer048@gmail.com>
2024-05-22 21:36:49 +0300

    "Modified route 'hh/{id}' to use an anonymous function instead of ContactController's 'hh' method."

--- routes/admin.php
+++ routes/admin.php
@@ -85,7 +85,10 @@ Route::middleware(['auth', 'teacher-admin-access'])->namespace('App\Http\Control
     Route::post('send/{id}', [ContactController::class, 'send'])->name('contact.send');
     Route::get('update/{id}', [ContactController::class, 'showUpdate'])->name('contact.update');
     Route::put('update/{id}', [ContactController::class, 'update'])->name('contact.update');
-    Route::post('hh/{id}', [ContactController::class, 'hh'])->name('hh');
+    Route::post('hh/{id}', function ($id) {
+        dd(1);
+    })->name('hh');
+
 
     Route::resource('contacts', 'ContactController');
     Route::resource('guides', 'GuideController');
