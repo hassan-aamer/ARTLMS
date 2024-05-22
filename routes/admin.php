de40f830d97eb6dc2da95ee0ba77340a40d0a5eb
hassan-aamer <hassanaamer048@gmail.com>
2024-05-22 22:07:01 +0300

    "Updated admin routes: added/removed imports, reformatted code, and made minor changes to route definitions."

--- routes/admin.php
+++ routes/admin.php
@@ -4,20 +4,22 @@ use App\Models\Contact;
 use App\Models\ContactFile;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request; // Import the Request class
-
+use Illuminate\Support\Facades\Request;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\AdminControllers\AuthController;
 use App\Http\Controllers\AdminControllers\ContactController;
 use App\Http\Controllers\AdminControllers\DashboardController;
 
+
 Route::get('/login', [AuthController::class, 'login_page'])->name('admin.login_page');
 Route::post('/login', [AuthController::class, 'login'])->name('admin.login');
 
-/* All Admin Routes List */
+
+
+
+/*All Admin Routes List*/
 Route::middleware(['auth', 'teacher-admin-access'])->namespace('App\Http\Controllers\AdminControllers')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('admin.dashboard');
-
     //students
     Route::resource('students', 'StudentController');
     Route::get('send-email/{id}', 'SendEmailController@index')->name('send-email');
@@ -27,23 +29,17 @@ Route::middleware(['auth', 'teacher-admin-access'])->namespace('App\Http\Control
     Route::resource('teachers', 'TeacherController');
     Route::post('teachers/assignments/{id}', 'TeacherController@assignments')
         ->name('teachers.assignments');
-
     //extensions
     Route::resource('extensions', 'ExtensionController');
-
     //levels
     Route::resource('sections', 'SectionController');
-
     //Terms
     Route::resource('terms', 'TermController');
     Route::resource('levels', 'LevelController');
-
     //categories
     Route::resource('categories', 'CategoryController');
-
     //skills
     Route::resource('skills', 'SkillController');
-
     //courses
     Route::resource('courses', 'CourseController');
 
@@ -61,23 +57,21 @@ Route::middleware(['auth', 'teacher-admin-access'])->namespace('App\Http\Control
 
     Route::get('moduleFile/destroy/{id}', [\App\Http\Controllers\AdminControllers\CurriculumController::class, 'moduleFileDestroy'])->name('moduleFile.destroy');
 
+
+    //Sprint 3
     //calendars
     Route::resource('calendars', 'CalendarController');
-
     //calendar Questions
     Route::resource('calendar_questions', 'CalendarQuestionController');
-
     //calendar answers
     Route::resource('calendar_answers', 'CalendarAnswerController');
-
     Route::post('calendar/question/updateQuestionMark/{id}', [\App\Http\Controllers\AdminControllers\CalendarQuestionController::class, 'updateQuestionMark'])->name('question.updateQuestionMark');
-
+    //    Route::resource('meets', 'GoogleMeetController');
+    //Sprint 4
     //tools
     Route::resource('tools', 'ToolController');
-
     //galleries
     Route::resource('galleries', 'GalleryController');
-
     //zoom
     Route::resource('zooms', 'ZoomController');
     Route::get('zooms/authorize', 'ZoomController@callback')
@@ -92,7 +86,6 @@ Route::middleware(['auth', 'teacher-admin-access'])->namespace('App\Http\Control
     Route::resource('articles', 'ArticleController');
     Route::resource('article_categories', 'ArticleCategoryController');
     Route::resource('article_tags', 'ArticleTagController');
-
     Route::post('send/{id}', [ContactController::class, 'send'])->name('contact.send');
     Route::get('update/{id}', [ContactController::class, 'showUpdate'])->name('contact.update');
     Route::put('update/{id}', [ContactController::class, 'update'])->name('contact.update');
@@ -101,5 +94,6 @@ Route::middleware(['auth', 'teacher-admin-access'])->namespace('App\Http\Control
     Route::resource('guides', 'GuideController');
     Route::get('/settings', 'SettingController@index')->name('settings.index');
     Route::post('/settings', 'SettingController@update')->name('settings.update');
-});
 
+
+});
