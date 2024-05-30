46a5b4336eaa59f6162f67c7a7a8507436406850
hassan-aamer <hassanaamer048@gmail.com>
2024-05-30 20:51:49 +0300

    "Added new form fields and tables for attaching files and links to contacts, and refactored existing code for iframe links and interactive links."

--- resources/views/admin_dashboard/contacts/attatch.blade.php
+++ resources/views/admin_dashboard/contacts/attatch.blade.php
@@ -20,44 +20,142 @@
                                     <form class="row g-3" id="validateForm" method="post" enctype="multipart/form-data"
                                         action="{{ route('att', $contact->id) }}">
                                         @csrf
-                                        @include('admin_dashboard.inputs.image')
-
-                                        <div class="container mt-5">
-                                            <div id="iframeLinksContainer" class="col-12">
-                                                <label class="form-label">رابط الفيديو (Iframe tag) 1</label>
-                                                <input type="text" name="link[]" class="form-control mb-2"
-                                                    placeholder="رابط الفيديو">
-                                            </div>
-                                            <button id="addIframeLinkButton" class="btn btn-primary">إضافة رابط آخر</button>
-                                        </div>
-
-                                        <div class="container mt-5">
-                                            <div id="interactiveLinksContainer" class="col-12">
-                                                <label class="form-label">رابط الفيديو تفاعلي 1</label>
-                                                <input type="text" name="url[]" class="form-control mb-2"
-                                                    placeholder="رابط الفيديو">
-                                            </div>
-                                            <button id="addInteractiveLinkButton" class="btn btn-primary">إضافة رابط
-                                                آخر</button>
+                                        <div class="col-12">
+                                            <label class="form-label">  العنوان <span class="text-danger">*</span> </label>
+                                            <input type="text" name="" class="form-control" required placeholder="ادخل عنوان العنصر">
                                         </div>
 
                                         <div class="col-12">
+                                            <label class="form-label"> وصف مختصر <span class="text-danger">*</span></label>
+                                            <textarea required name="" class="form-control ckeditor" placeholder="وصف مختصر" rows="4" cols="4"></textarea>
+                                        </div>
+
+
+                                        @include('admin_dashboard.inputs.image')
+
+
+                                        <div class="col-12 mb-5">
+                                            <div class="float-end mb-2">
+                                                <button type="button" class="btn btn-sm btn-success" id="addNewRow"> اضف رابط اخر</button>
+                                            </div>
+                                            <table class="no-datatable table table-striped table-hover table-responsive table-bordered mb-0">
+                                                <thead>
+                                                    <th> رابط فيديو (Iframe tag) 1  </th>
+                                                    <th> حذف</th>
+                                                </thead>
+                                                <tbody id="lines">
+                                                    <tr id="tr">
+                                                        <td>
+                                                            <input class="form-control" name="link[]" placeholder=" رابط الفيديو" required />
+                                                        </td>
+                                                        <td>
+                                                            <button type="button" class="btn btn-sm btn-danger removeRow">
+                                                                <i class="lni lni-trash m-0"></i>
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+
+
+
+
+                                        <div class="col-12 mb-5">
+                                            <div class="float-end mb-2">
+                                                <button type="button" class="btn btn-sm btn-success" id="addNewRow"> اضف رابط اخر</button>
+                                            </div>
+                                            <table class="no-datatable table table-striped table-hover table-responsive table-bordered mb-0">
+                                                <thead>
+                                                    <th> رابط فيديو تفاعلى</th>
+                                                    <th> حذف</th>
+                                                </thead>
+                                                <tbody id="lines">
+                                                    <tr id="tr">
+                                                        <td>
+                                                            <input class="form-control" name="url[]" placeholder=" رابط الفيديو" required />
+                                                        </td>
+                                                        <td>
+                                                            <button type="button" class="btn btn-sm btn-danger removeRow">
+                                                                <i class="lni lni-trash m-0"></i>
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+
+                                        {{-- <div class="col-12">
                                             <label for="form-label">إضافة ملفات</label>
                                             <input class="form-control" id="triggerInputt" type="file" name="file"
                                                 placeholder="إضافة ملفات" accept="application/pdf">
-                                        </div>
+                                        </div> --}}
 
 
                                         <div class="col-12">
-                                            <label for="titleInput"> وصف الميتا الخاص بال SEO</label>
-                                            <input class="form-control" id="titleInput" type="text" name="title"
-                                                placeholder="إضافة وصف النطاق">
+                                            <label for="form-label"> وصف الميتا الخاص بال SEO</label>
+                                            <textarea required name="title" class="form-control " placeholder=" وصف الميتا" rows="4" cols="4"></textarea>
                                         </div>
+
                                         <div class="col-12">
                                             <label for="titleInput"> الكلمات الدلالية الخاصة بال SEO</label>
                                             <input class="form-control" id="titleInput" type="text" name="description"
                                                 placeholder="إضافة وصف النطاق">
                                         </div>
+
+
+
+
+
+
+
+                                        <div class="col-12 mb-5">
+
+                                            <h5 class="mt-4 mb-3">  ارفع الملفات الخاصه بالعنصر : <span class="text-danger">*</span>  </h5>
+
+
+                                            <div class="float-end mb-2">
+                                                <button type="button" class="btn btn-sm btn-success" id="addNewRow">أضف ملف أخر</button>
+                                            </div>
+                                            <table class="no-datatable table table-striped table-hover table-responsive table-bordered mb-0">
+                                                <thead>
+                                                    <th>اسم الملف</th>
+                                                    <th>نوع الملف</th>
+                                                    <th> الملف</th>
+                                                    <th> توصيف الملف (Meta)</th>
+                                                    <th> حذف</th>
+                                                </thead>
+                                                <tbody id="lines">
+                                                    <tr id="tr">
+                                                        <td>
+                                                            <input class="form-control" name="" placeholder="ادخل اسم الملف" required />
+                                                        </td>
+                                                        <td>
+                                                            <select class="form-control" name="" required>
+                                                                <option value=""> اختر نوع الملف </option>
+                                                                @foreach(extensions() as $ext)
+                                                                    <option value="{{$ext->file_type .' - '.$ext->file_ext}}"> {{$ext->file_type .' - '.$ext->file_ext}} </option>
+                                                                @endforeach
+                                                            </select>
+                                                        </td>
+                                                        <td>
+                                                            <input type="file" class="form-control" name="file"  required />
+                                                        </td>
+                                                        <td>
+                                                            <input class="form-control" name="" placeholder="ادخل  توصيف الملف (Meta)"  />
+                                                        </td>
+                                                        <td>
+                                                            <button type="button" class="btn btn-sm btn-danger removeRow">
+                                                                <i class="lni lni-trash m-0"></i>
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+
+
+                                        </div>
+
 
 
                                         @include('admin_dashboard.inputs.add_btn')
@@ -104,4 +202,10 @@
             container.appendChild(newInput);
         });
     </script>
+
+
+
+
+
+
 @endpush
