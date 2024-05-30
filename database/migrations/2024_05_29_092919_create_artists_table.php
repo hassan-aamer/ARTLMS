5072cf16101aaedbbe84a5aba17f6b40fdd05b96
hassan-aamer <hassanaamer048@gmail.com>
2024-05-30 20:05:53 +0300

    "Added user_id foreign key to artists table and removed separate migration for adding user_id column"

--- database/migrations/2024_05_29_092919_create_artists_table.php
+++ database/migrations/2024_05_29_092919_create_artists_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('artists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
